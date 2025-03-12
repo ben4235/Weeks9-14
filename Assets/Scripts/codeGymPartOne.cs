7b51511ebb84572011659bb257a32a35bb486193
Benjamin Piatkowski <58381976+ben4235@users.noreply.github.com>
2025-03-12 12:01:53 -0400

    code gym week 9 part one completed

--- Assets/Scripts/codeGymPartOne.cs
+++ Assets/Scripts/codeGymPartOne.cs
@@ -6,25 +6,42 @@ using UnityEngine.UIElements;
 
 public class codeGymPartOne : MonoBehaviour
 {
-    public UnityEvent pointerEnter;
-    public UnityEvent pointerExit;
-    public UnityEvent pointerClick;
+    public RectTransform image;
+    public GameObject prefabSpawn;
+    public UnityEvent mouseClicked;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) // Left click
+        {
+            // Get mouse position in screen space
+            Vector3 mouseScreenPos = Input.mousePosition;
+
+            // Convert screen position to world position
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mouseScreenPos.x, mouseScreenPos.y, 10f)); // Adjust Z depth
+
+            // Instantiate prefab at the calculated world position
+            Instantiate(prefabSpawn, worldPos, Quaternion.identity);
+        }
     }
 
     public void mouseJustEntered()
     {
-        
+        image.localScale = Vector3.one * 1.2f;
     }
+
+    public void mouseJustLeft()
+    {
+        image.localScale = Vector3.one;
+    }
+
+
 
 }
