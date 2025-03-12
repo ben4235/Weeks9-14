ff90d7fbdc673b6854317a4a10f71d76531fb1cb
Benjamin Piatkowski <58381976+ben4235@users.noreply.github.com>
2025-03-12 11:18:02 -0400

    started work on the code gym week 9

---
+++ Assets/Scripts/codeGymPartOne.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+public class codeGymPartOne : MonoBehaviour
+{
+    public UnityEvent pointerEnter;
+    public UnityEvent pointerExit;
+    public UnityEvent pointerClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+}
