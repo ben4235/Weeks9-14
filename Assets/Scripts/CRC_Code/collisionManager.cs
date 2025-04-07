30af0523e1871ef3928b0c362172b9d05f8bc77a
ben4235 <bpiatkowski02@gmail.com>
2025-04-07 15:17:34 -0400

    started the collision manager script

--- Assets/Scripts/CRC_Code/collisionManager.cs
+++ Assets/Scripts/CRC_Code/collisionManager.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class collisionManager : MonoBehaviour
 {
+    private playerController player;
+    private bullet bullet;
+    private enemyManager enemy;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,10 @@ public class collisionManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void bulletEnemyCollision()
+    {
+
     }
 }
